8406677d129807b3afcb3cf729b0c2829fa67707
tom837 <71010075+tom837@users.noreply.github.com>
2024-02-12 14:48:45 +0100

    Add files via upload

---
+++ model_creation.py
@@ -0,0 +1,21 @@
+import tensorflow as tf
+
+#create and compile model
+def create_model(in_shape,output_shape):
+    model= tf.keras.models.Sequential([
+        tf.keras.layers.LSTM(units = 50,return_sequences=True, input_shape=(in_shape[0],in_shape[1]),kernel_regularizer=tf.keras.regularizers.l1(l1=0.01)),
+        tf.keras.layers.Dropout(0.3),
+        tf.keras.layers.GRU(units=50, return_sequences=False, kernel_regularizer=tf.keras.regularizers.l2(l2=0.01)),
+        tf.keras.layers.Dense(output_shape)])
+
+    model.compile(optimizer='adam', loss='mean_squared_error')
+    model.summary()
+    return model
+
+#for now we are just using close of the last 60 days to predict next day's close
+inp_sp=[90,1]
+out_sp=1
+model=create_model(inp_sp,out_sp)
+
+#save model
+model.save("predictor5")
