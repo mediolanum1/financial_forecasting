f879cca34e825da92378f2687e5bb46f5dc0cfa8
tom837 <71010075+tom837@users.noreply.github.com>
2024-02-12 20:36:51 +0100

    Update and rename generation.py to generation_singleday.py

--- stock price predictor naive lstm/generation_singleday.py
+++ stock price predictor naive lstm/generation_singleday.py
@@ -29,28 +29,6 @@ def get_data(strt,seq_length,data_length,dataset):
         validation.append(dataset[i+seq_length])
     return training,validation
 
-"""
-#if you want to have to 30 day prediction as oposed to day by day prediction comment out next part and uncomment this part
-seq_len=60
-dataset_len=1
-trainining=[]
-trainining,correct=get_data(len(dataset[company])-260,seq_len,dataset_len,dataset[company])
-date=[]
-for i in range(30):
-    date.append(i)
-#predict the next "epochs" days could skip this part if we want just day by day comparison
-def predict(training, model,epochs):
-    out=[]
-    for i in range(epochs):
-        trainining= tf.constant(training)
-        prediction=model.predict(trainining)
-        out.append(prediction[0][0])
-        training[0].append(float(prediction[0][0]))
-        training[0].remove(training[0][0])
-    return out
-prediction=predict(trainining, model, 30)
-"""
-
 
 #if you want to have day by day prediction as oposed to 30 day prediction comment out previous part and uncomment this part
 seq_len=60
