305f7539de3ae308b68f821858ee28822bc040fa
tom837 <71010075+tom837@users.noreply.github.com>
2024-02-12 18:10:39 +0100

    Add files via upload

---
+++ stock price predictor naive lstm/generation.py
@@ -0,0 +1,97 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+model=tf.keras.models.load_model("predictor4")
+
+company=0
+
+#get dataset
+dataset=[]
+df = pd.read_csv('AAPL.csv')
+dataset.append(df["close"].tolist())
+df = pd.read_csv('GOOG.csv')
+dataset.append(df["Close"].tolist())
+df = pd.read_csv('NFLX.csv')
+dataset.append(df["Close"].tolist())
+df = pd.read_csv('Tesla.csv')
+dataset.append(df["Close"].tolist())
+
+#same as in main except we only take the data for each sepetate company at a time
+def get_data(strt,seq_length,data_length,dataset):
+    training=[]
+    validation=[]
+    for i in range(strt,strt+data_length):
+        tmp=[]
+        for instance in dataset[i:i+seq_length]:
+            tmp.append(instance)
+        training.append(tmp)
+        validation.append(dataset[i+seq_length])
+    return training,validation
+
+"""
+#if you want to have to 30 day prediction as oposed to day by day prediction comment out next part and uncomment this part
+seq_len=60
+dataset_len=1
+trainining=[]
+trainining,correct=get_data(len(dataset[company])-260,seq_len,dataset_len,dataset[company])
+date=[]
+for i in range(30):
+    date.append(i)
+#predict the next "epochs" days could skip this part if we want just day by day comparison
+def predict(training, model,epochs):
+    out=[]
+    for i in range(epochs):
+        trainining= tf.constant(training)
+        prediction=model.predict(trainining)
+        out.append(prediction[0][0])
+        training[0].append(float(prediction[0][0]))
+        training[0].remove(training[0][0])
+    return out
+prediction=predict(trainining, model, 30)
+"""
+
+
+#if you want to have day by day prediction as oposed to 30 day prediction comment out previous part and uncomment this part
+seq_len=60
+dataset_len=200
+trainining=[]
+trainining,correct=get_data(len(dataset[company])-(200+seq_len),seq_len,dataset_len,dataset[company])
+date=[]
+for i in range(dataset_len):
+    date.append(i)
+prediction=model.predict(trainining)
+
+
+#plot the predicted prices compared to the actual prices
+plt.figure()
+plt.plot(date, prediction, label='predictions')
+plt.plot(date, dataset[company][len(dataset[company])-200:len(dataset[company])-(200-len(date))], label='actual')
+plt.title('prediction vs actual')
+plt.xlabel('date')
+plt.ylabel('prices')
+plt.legend()
+plt.show()
+
+#simulate earnings
+
+test_correct= dataset[company][len(dataset[company])-200:len(dataset[company])-(200-len(date))]
+
+money=10000
+
+for i in range(1,len(prediction)):
+  estimated_gain=prediction[i][0] - test_correct[i-1]
+  if estimated_gain>0:
+    no_share=money/test_correct[i-1]
+    money=no_share*test_correct[i]
+print(money)
+
+money_ideal=10000
+for i in range(1,len(prediction)):
+  estimated_gain=test_correct[i]-test_correct[i-1]
+  if estimated_gain>0:
+    no_share=money_ideal/test_correct[i-1]
+    money_ideal=no_share*test_correct[i]
+
+print(money_ideal)
+
---
+++ stock price predictor naive lstm/main.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import random
+
+#get the whole dataset 
+dataset=[]
+df = pd.read_csv('AAPL.csv')
+dataset.append(df["close"].tolist())
+df = pd.read_csv('GOOG.csv')
+dataset.append(df["Close"].tolist())
+df = pd.read_csv('NFLX.csv')
+dataset.append(df["Close"].tolist())
+df = pd.read_csv('Tesla.csv')
+dataset.append(df["Close"].tolist())
+
+#import the model
+model=tf.keras.models.load_model("predictor4")
+
+#get all the data partitioned into (seq_long) samples (except the last 200 we keep for testing later to insure no overfiting)
+def get_data(seq_length,dataset):
+    training=[]
+    validation=[]
+    for i in range(len(dataset)):
+        for j in range(len(dataset[i])-(200+seq_length)):
+            tmp=[]
+            for instance in dataset[i][j:j+seq_length]:
+                tmp.append(instance)
+            training.append(tmp)
+            validation.append(dataset[i][j+seq_length])
+    return training,validation
+
+seq_len=60
+trainining_tmp,correct_tmp=get_data(seq_len,dataset)
+
+#partition dataset into training sample and validation
+indices = list(range(len(correct_tmp)))
+random.shuffle(indices)
+split = int(len(indices) * 0.7)
+trainining= [trainining_tmp[i] for i in indices[:split]]
+correct= [correct_tmp[i] for i in indices[:split]]
+trainining=tf.constant(trainining)
+correct=tf.constant(correct)
+val= [trainining_tmp[i] for i in indices[split:]]
+val_cor= [correct_tmp[i] for i in indices[split:]]
+val=tf.constant(val)
+val_cor=tf.constant(val_cor)
+
+#print(tf.shape(trainining))
+#print(tf.shape(correct))
+
+#train the model on the data and save it (batch_size and epochs can be played around with to find the ideal numbers)
+history = model.fit(trainining, correct, epochs=300, batch_size=32, validation_data=(val,val_cor))
+model.save("predictor4")
+
+
+
+
